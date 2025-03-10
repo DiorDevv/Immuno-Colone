96f88dea8588b8a8abc9dc3a9c3ecf287387383c
Diyorbek <diyorbekaxmadjonov98@gamil.com>
2025-03-10 15:55:15 +0500

    Bemor app

--- bemor/admin.py
+++ bemor/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ExportMixin
-from .models import Manzil, OperatsiyaBolganJoy, Jinsi, BemorningHolati, Bemor
+from .models import Manzil, OperatsiyaBolganJoy, Jinsi, BemorningHolati, Bemor, BemorQoshsih
 
 
 class BemorInline(admin.TabularInline):  # Bemorlarni boshqa adminlarda ichki jadval sifatida ko‘rsatish
@@ -20,7 +20,9 @@ class ManzilAdmin(ExportMixin, admin.ModelAdmin):  # Export funksiyasi qo‘shil
 
 @admin.register(OperatsiyaBolganJoy)
 class OperatsiyaBolganJoyAdmin(ExportMixin, admin.ModelAdmin):
-    list_display = ('mamlakat', 'operatsiya_bolgan_joy', 'transplantatsiya_sana', 'operatsiya_oxirlangan_sana', 'ishlatilgan_miqdor')
+    list_display = (
+        'mamlakat', 'operatsiya_bolgan_joy', 'transplantatsiya_sana', 'operatsiya_oxirlangan_sana',
+        'ishlatilgan_miqdor')
     search_fields = ('mamlakat', 'operatsiya_bolgan_joy')
     list_filter = ('mamlakat', 'transplantatsiya_sana')
     ordering = ('transplantatsiya_sana',)
@@ -44,27 +46,41 @@ class BemorningHolatiAdmin(ExportMixin, admin.ModelAdmin):
     list_per_page = 20
 
 
+from django.contrib import admin
+from import_export.admin import ExportMixin
+from .models import BemorQoshsih, Bemor
+
+
+@admin.register(BemorQoshsih)
+class BemorQoshsihAdmin(admin.ModelAdmin):
+    list_display = ("JSHSHIR", "ism", "familiya", "tugilgan_sana", "jinsi")
+    search_fields = ("JSHSHIR", "ism", "familiya")
+    list_filter = ("jinsi", "tugilgan_sana")
+    date_hierarchy = "tugilgan_sana"
+    ordering = ("tugilgan_sana",)
+    list_per_page = 20
+
+
 @admin.register(Bemor)
 class BemorAdmin(ExportMixin, admin.ModelAdmin):
-    list_display = ('ism', 'familiya', 'tugilgan_sana', 'jinsi', 'manzil', 'bemor_holati', 'arxivga_olingan_sana')
-    search_fields = ('ism', 'familiya', 'JSHSHIR')
-    list_filter = ('jinsi', 'tugilgan_sana', 'arxivga_olingan_sana')
-    date_hierarchy = 'tugilgan_sana'
-    autocomplete_fields = ('jinsi', 'manzil', 'bemor_holati', 'operatsiya_bolgan_joy')
-    ordering = ('tugilgan_sana',)
+    list_display = ("bemor", "manzil", "bemor_holati", "operatsiya_bolgan_joy", "arxivga_olingan_sana")
+    search_fields = ("bemor__JSHSHIR", "bemor__ism", "bemor__familiya")
+    list_filter = ("bemor__jinsi", "bemor_holati", "arxivga_olingan_sana")
+    date_hierarchy = "arxivga_olingan_sana"
+    autocomplete_fields = ("bemor", "manzil", "bemor_holati", "operatsiya_bolgan_joy")
+    ordering = ("arxivga_olingan_sana",)
     list_per_page = 20
     fieldsets = (
-        ('Shaxsiy ma’lumotlar', {
-            'fields': ('JSHSHIR', 'ism', 'familiya', 'tugilgan_sana', 'jinsi')
+        ("Bemor ma’lumotlari", {
+            "fields": ("bemor",)
         }),
-        ('Qo‘shimcha ma’lumotlar', {
-            'fields': ('manzil', 'bemor_holati', 'operatsiya_bolgan_joy', 'qoshimcha_malumotlar'),
-            'classes': ('collapse',)
+        ("Qo‘shimcha ma’lumotlar", {
+            "fields": ("manzil", "bemor_holati", "operatsiya_bolgan_joy", "qoshimcha_malumotlar"),
+            "classes": ("collapse",)
         }),
-        ('Arxiv', {
-            'fields': ('arxivga_olingan_sana', 'biriktirilgan_file'),
-            'classes': ('collapse',)
+        ("Arxiv", {
+            "fields": ("arxivga_olingan_sana", "biriktirilgan_file"),
+            "classes": ("collapse",)
         }),
     )
-    readonly_fields = ('arxivga_olingan_sana',)  # O‘zgartirib bo‘lmaydigan maydon
-
+    readonly_fields = ("arxivga_olingan_sana",)
--- bemor/models.py
+++ bemor/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 from shared.models import BaseModel
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
+User = get_user_model()
 
 
 class Manzil(BaseModel):
@@ -23,6 +27,7 @@ class OperatsiyaBolganJoy(BaseModel):
     mamlakat = models.CharField(max_length=255)
     operatsiya_bolgan_joy = models.CharField(max_length=255)
     transplantatsiya_sana = models.DateField()
+    transplantatsiya_operatsiyasi = models.CharField(max_length=255)
     operatsiya_oxirlangan_sana = models.DateField()
     ishlatilgan_miqdor = models.PositiveSmallIntegerField()
 
@@ -61,29 +66,50 @@ class BemorningHolati(BaseModel):
         return self.holati
 
 
-class Bemor(BaseModel):
-    JSHSHIR = models.CharField(max_length=30)
+class BemorQoshsih(BaseModel):
+    JSHSHIR = models.CharField(
+        max_length=14,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{14}$',
+                message="JSHSHIR faqat 14 ta raqamdan iborat bo‘lishi kerak!"
+            )
+        ]
+    )
     ism = models.CharField(max_length=255)
     familiya = models.CharField(max_length=255)
     tugilgan_sana = models.DateField()
     jinsi = models.ForeignKey(Jinsi, on_delete=models.CASCADE)
-    manzil = models.ForeignKey(Manzil, on_delete=models.SET_NULL, null=True, blank=True)
-    bemor_holati = models.ForeignKey(BemorningHolati, on_delete=models.CASCADE, null=True, blank=True)
-    operatsiya_bolgan_joy = models.ForeignKey(OperatsiyaBolganJoy, on_delete=models.CASCADE, null=True, blank=True)
-    biriktirilgan_file = models.FileField(upload_to='biriktirilgan/%Y/%m/%d', null=True, blank=True)
-    qoshimcha_malumotlar = models.TextField(null=True, blank=True)
-    # biriktirilgan_shiforkot = models.ForeignKey(SHifokor, blank=True, null=True)
-    arxivga_olingan_sana = models.DateTimeField(null=True, blank=True)
 
     class Meta:
-        verbose_name = "Bemor"
-        verbose_name_plural = "Bemorlar"
+        verbose_name = "Bemor qo'shish"
+        verbose_name_plural = "Bemorlar qo'shish"
         constraints = [
             models.UniqueConstraint(fields=['ism', 'familiya', 'tugilgan_sana'], name='unique_bemor')
         ]
 
     def __str__(self):
-        return f"Bemornig ismi {self.ism}, Familiya {self.familiya}"
+        return self.JSHSHIR
 
-    def sharifi(self):
-        return f"Bemornig sharifi {self.ism} {self.familiya}"
+
+class Bemor(BaseModel):
+    bemor = models.ForeignKey(BemorQoshsih, on_delete=models.CASCADE)
+    manzil = models.ForeignKey(Manzil, on_delete=models.SET_NULL, null=True, blank=True)
+    bemor_holati = models.ForeignKey(BemorningHolati, on_delete=models.CASCADE, null=True, blank=True)
+    operatsiya_bolgan_joy = models.ForeignKey(OperatsiyaBolganJoy, on_delete=models.CASCADE, null=True, blank=True)
+    biriktirilgan_file = models.FileField(upload_to='biriktirilgan/%Y/%m/%d', null=True, blank=True)
+    qoshimcha_malumotlar = models.TextField(null=True, blank=True)
+    arxivga_olingan_sana = models.DateTimeField(null=True, blank=True)
+
+    # def clean(self):
+    #     if not self.bemor.JSHSHIR:
+    #         raise ValidationError("Bemor qo‘shish uchun JSHSHIR mavjud bo‘lishi kerak!")
+    #
+    # def save(self, *args, **kwargs):
+    #     self.clean()  # Validatsiyani ishga tushiramiz
+    #     super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Bemor"
+        verbose_name_plural = "Bemorlar"
